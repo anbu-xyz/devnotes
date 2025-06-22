979e7f88a810f4862cd4e6ca0175a77305ffad24
anbu <git@anbu.work>
2025-06-22 08:58:26 +0100

    improve search - if the result has just one entry, redirect to that markdown file

--- src/main/java/uk/anbu/devnotes/controller/SearchController.java
+++ src/main/java/uk/anbu/devnotes/controller/SearchController.java
@@ -1,11 +1,13 @@
 package uk.anbu.devnotes.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import gg.jte.TemplateEngine;
 import gg.jte.TemplateOutput;
 import gg.jte.output.StringOutput;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,8 +15,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import uk.anbu.devnotes.module.SearchExecutor;
 import uk.anbu.devnotes.service.ConfigService;
+import uk.anbu.devnotes.types.SearchResult;
 
 import java.util.HashMap;
+import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -28,7 +32,7 @@ public class SearchController {
     @GetMapping("/search")
     public ResponseEntity<String> search(@RequestParam("q") String searchParam,
           @RequestParam(value = "caseSensitive", required = false, defaultValue = "false") boolean caseSensitive,
-          @RequestParam(value="renderHtmlResults", required = false, defaultValue = "false") boolean renderHtmlResults) {
+          @RequestParam(value= "json", required = false, defaultValue = "false") boolean renderJsonResults) {
         try {
             searchParam = searchParam.trim();
             var result = new SearchExecutor(configService.getDocsDirectory())
@@ -36,26 +40,41 @@ public class SearchController {
             if (result.isEmpty()) {
                 return ResponseEntity.notFound().build();
             } else {
-                if (renderHtmlResults) {
-                    TemplateOutput output = new StringOutput();
-                    var params = new HashMap<String, Object>();
-                    params.put("results", result);
-                    params.put("searchTerm", searchParam);
-                    templateEngine.render("tools/search-results.jte", params, output);
-                    return ResponseEntity.ok()
-                            .contentType(org.springframework.http.MediaType.TEXT_HTML)
-                            .body(output.toString());
+                if (renderJsonResults) {
+                    return renderJsonResults(result);
                 } else {
-                    var jsonOutput = objectMapper.writeValueAsString(result);
-                    return ResponseEntity.ok()
-                            .contentType(org.springframework.http.MediaType.APPLICATION_JSON)
-                            .body(jsonOutput);
+                    return renderHtmlResults(searchParam, result);
                 }
             }
         } catch (Exception e) {
             log.error("Error searching", e);
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body("Error searching for " + searchParam);
+        }
+    }
+
+    private ResponseEntity<String> renderJsonResults(List<SearchResult> result) throws JsonProcessingException {
+        var jsonOutput = objectMapper.writeValueAsString(result);
+        return ResponseEntity.ok()
+                .contentType(org.springframework.http.MediaType.APPLICATION_JSON)
+                .body(jsonOutput);
+    }
+
+    private ResponseEntity<String> renderHtmlResults(String searchParam, List<SearchResult> result) {
+        TemplateOutput output = new StringOutput();
+        var params = new HashMap<String, Object>();
+        params.put("results", result);
+        params.put("searchTerm", searchParam);
+        templateEngine.render("tools/search-results.jte", params, output);
+        if (result.size() == 1) {
+            return ResponseEntity
+                    .status(HttpStatus.FOUND)
+                    .header(HttpHeaders.LOCATION, "/markdown?filename=" + result.get(0).fileName())
+                    .build();
+        } else {
+            return ResponseEntity.ok()
+                    .contentType(org.springframework.http.MediaType.TEXT_HTML)
+                    .body(output.toString());
         }
     }
 
