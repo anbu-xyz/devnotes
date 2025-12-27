754319d8033f1a00e47af330897939b96efb6512
anbu <git@anbu.uk>
2025-12-27 19:33:48 +0000

    Created an awesome toolbar page to aid discoverability of its features

--- TODO.md
+++ TODO.md
@@ -2,8 +2,10 @@
 
 ## Bugs
 * Sorted column name should be stored in output file
+* Sometimes I've noticed editing sql using the widget duplicates it.
 
 ## Features
+* Enable security using preconfigured IPs
 * Allow pasting images into markdown files
 * Add a way to run parameterized queries
 * Autodetect a file being edited in another tab and stop new edits
--- src/main/java/uk/anbu/devnotes/controller/AwesomeBarController.java
+++ src/main/java/uk/anbu/devnotes/controller/AwesomeBarController.java
@@ -1,20 +1,37 @@
 package uk.anbu.devnotes.controller;
 
+import gg.jte.TemplateEngine;
+import gg.jte.TemplateOutput;
+import gg.jte.output.StringOutput;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import uk.anbu.devnotes.module.CommandExecutor;
 import uk.anbu.devnotes.types.Command;
 
+import java.util.HashMap;
+
 @RestController
 @RequiredArgsConstructor
 @Slf4j
 public class AwesomeBarController {
-
     private final CommandExecutor commandExecutor;
+
+    private final TemplateEngine templateEngine;
+
+    @GetMapping("/awesome-bar")
+    public ResponseEntity<String> awesomeBar() {
+        var model = new HashMap<String, Object>();
+        TemplateOutput output = new StringOutput();
+        templateEngine.render("tools/awesome-bar.jte", model, output);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(output.toString());
+    }
 
     @GetMapping("/awesome-bar/command")
     public ResponseEntity<String> postAwesomeBar(@RequestParam("q") String command) {
@@ -25,4 +42,18 @@ public class AwesomeBarController {
         return commandExecutor.executeCommand(new Command(firstWord, restOfCommand));
     }
 
+    @PostMapping("/awesome-bar/todo")
+    public ResponseEntity<String> addTodo(@RequestParam("todoText") String todoString) {
+        return commandExecutor.executeCommand(new Command("todo", todoString));
+    }
+
+    @PostMapping("/awesome-bar/search")
+    public ResponseEntity<String> search(@RequestParam("searchText") String searchText) {
+        return commandExecutor.executeCommand(new Command("search", searchText));
+    }
+
+    @PostMapping("/awesome-bar/searchLocation")
+    public ResponseEntity<String> searchLocation(@RequestParam("searchLocationText") String searchLocationText) {
+        return commandExecutor.executeCommand(new Command("searchLocationText", searchLocationText));
+    }
 }
